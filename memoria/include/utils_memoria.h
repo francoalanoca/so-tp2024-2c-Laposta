5fe7a8e2da9f921b0cb150e44541084d14a8fb29
Franco Alanoca <falanocavillarreal@frba.utn.edu.ar>
2024-08-30 09:35:17 +0000

    Inicio protocolos

--- memoria/include/utils_memoria.h
+++ memoria/include/utils_memoria.h
@@ -7,6 +7,48 @@
 
 //----------------------------------Estructuras---------------------------------
 
+typedef struct{
+    uint32_t pid;                     //pcb del proceso
+    char *archivo_pseudocodigo;     //nombre del proceso
+} t_m_crear_proceso;
+
+
+
+typedef struct{
+
+    /* data */
+
+}t_m_crear_hilo;
+
+
+
+
+typedef struct{
+
+    /* data */
+
+} t_m_contexto;
+
+
+typedef struct{
+    uint32_t pid;
+    uint32_t program_counter;
+}t_proceso_memoria;
+
+
+typedef struct{
+    uint32_t pid;
+    uint32_t direccion_fisica;
+    uint32_t tamanio;
+    char* valor;
+} t_escribir_leer;
+
+
+typedef struct{
+
+    /* data */
+
+} t_dump;
 
 
 //----------------------------------Variables Externs-------------------------
@@ -14,6 +56,19 @@
 
 
 //----------------------------------Prototipos---------------------------------
+
+t_m_crear_proceso* deserializar_iniciar_proceso(t_list*  lista_paquete );
+void enviar_respuesta_iniciar_proceso(t_m_crear_proceso* crear_proceso ,int socket_kernel);
+uint32_t deserializar_finalizar_proceso(valores);
+void enviar_respuesta_finalizar_proceso(pid_proceso_a_finalizar, socket_kernel);
+
+
+t_proceso_memoria* deserializar_solicitud_instruccion(valores);
+void enviar_respuesta_instruccion(instruccion, socket_cliente);
+t_escribir_leer* deserializar_read_memoria(t_list*  lista_paquete );
+void enviar_respuesta_read_memoria(void* respuesta_leer, int socket_cpu);
+t_escribir_leer* deserializar_write_memoria(t_list*  lista_paquete);
+void enviar_respuesta_write_memoria(char* respuesta_escribir, int socket_cliente);
 
 
 #endif /* MEMORIA_H */
--- memoria/src/protocolo.c
+++ memoria/src/protocolo.c
@@ -14,6 +14,7 @@ void memoria_atender_cpu(){
         //Se queda esperando a que Cpu le envie algo y extrae el cod de operacion
 		int cod_op = recibir_operacion(socket_cpu);
 		op_code response;
+		t_list* valores =  malloc(sizeof(t_list));
 
 
 		switch (cod_op) {
@@ -27,8 +28,52 @@ void memoria_atender_cpu(){
             }
             break;
 		
+		case SOLICITUD_CONTEXTO:
+			log_info(logger_memoria, "Recibí SOLICITUD_CONTEXTO \n");
+			valores = recibir_paquete(socket_cpu);
+			usleep(cfg_memoria->RETARDO_RESPUESTA * 1000);
+			log_info(logger_memoria, "enviada respuesta de SOLICITUD_CONTEXTO_RTA \n");
+			break;
+
+		case SOLICITUD_INSTRUCCION:
+			log_info(logger_memoria, "Recibí SOLICITUD_INSTRUCCION \n");
+			valores = recibir_paquete(socket_cpu);
+			//t_proceso_memoria* solicitud_instruccion = deserializar_solicitud_instruccion(valores);         
+            //char* instruccion = buscar_instruccion(solicitud_instruccion->pid, solicitud_instruccion->program_counter);
+			usleep(cfg_memoria->RETARDO_RESPUESTA * 1000);
+			//enviar_respuesta_instruccion(instruccion, socket_cpu);    
+			log_info(logger_memoria, "enviada respuesta de SOLICITUD_INSTRUCCION_RTA \n");
+			break;
+
+		case READ_MEMORIA:
+			log_info(logger_memoria, "Recibí READ_MEMORIA \n");
+			valores = recibir_paquete(socket_cpu);
+			//t_escribir_leer* peticion_leer = deserializar_read_memoria(valores);     
+            //char* respuesta_leer = leer_memoria(peticion_leer->pid, peticion_leer->direccion_fisica, peticion_leer->tamanio);
+			usleep(cfg_memoria->RETARDO_RESPUESTA * 1000);
+			//enviar_respuesta_read_memoria(respuesta_leer, socket_cpu);
+			log_info(logger_memoria, "enviada respuesta de READ_MEMORIA_RTA \n");
+			break;
+
+		case WRITE_MEMORIA:
+			log_info(logger_memoria, "Recibí WRITE_MEMORIA \n");
+			valores = recibir_paquete(socket_cpu);
+			//t_escribir_leer* peticion_escribir = deserializar_write_memoria(valores);   
+            //char* respuesta_escribir = escribir_memoria(peticion_escribir->pid, peticion_escribir->direccion_fisica, peticion_escribir->valor, peticion_escribir->tamanio);
+			usleep(cfg_memoria->RETARDO_RESPUESTA * 1000);
+			//enviar_respuesta_write_memoria(respuesta_escribir, socket_cpu);
+			log_info(logger_memoria, "enviada respuesta de WRITE_MEMORIA_RTA \n");
+			break;
+		
+		case DEVOLUCION_CONTEXTO:
+			log_info(logger_memoria, "Recibí DEVOLUCION_CONTEXTO \n");
+			valores = recibir_paquete(socket_cpu);
+			usleep(cfg_memoria->RETARDO_RESPUESTA * 1000);
+			log_info(logger_memoria, "enviada respuesta de DEVOLUCION_CONTEXTO_RTA \n");
+			break;
+
 		case -1:
-			log_error(logger_memoria, "CPU se desconecto. Terminando servidor.");
+			log_error(logger_memoria, "CPU se desconecto. Terminando servidor.\n");
 			//return EXIT_FAILURE;
 			break;
 
@@ -52,6 +97,7 @@ void memoria_atender_kernel(){
         //Se queda esperando a que KErnel le envie algo y extrae el cod de operacion
 		int cod_op = recibir_operacion(socket_kernel);
 		op_code response;
+		t_list* valores =  malloc(sizeof(t_list));
 
 
 		switch (cod_op) {
@@ -65,8 +111,50 @@ void memoria_atender_kernel(){
             }
             break;
 		
+		case INICIAR_PROCESO:
+			log_info(logger_memoria, "Recibí INICIAR_PROCESO \n");
+			valores = recibir_paquete(socket_kernel);
+			//t_m_crear_proceso iniciar_proceso = deserializar_iniciar_proceso(valores);
+			//leer_instrucciones(iniciar_proceso->archivo_pseudocodigo, iniciar_proceso->pid);
+			//crear_proceso(iniciar_proceso->pid);
+			usleep(cfg_memoria->RETARDO_RESPUESTA * 1000);
+			//enviar_respuesta_iniciar_proceso(iniciar_proceso, socket_kernel);
+			log_info(logger_memoria, "enviada respuesta de INICIAR_PROCESO_RTA \n");
+			break;
+
+		case FINALIZAR_PROCESO:
+			log_info(logger_memoria, "Recibí FINALIZAR_PROCESO \n");
+			valores = recibir_paquete(socket_kernel);
+			//uint32_t pid_proceso_a_finalizar = deserializar_finalizar_proceso(valores);
+            //finalizar_proceso(pid_proceso_a_finalizar);
+			usleep(cfg_memoria->RETARDO_RESPUESTA * 1000);
+			//enviar_respuesta_finalizar_proceso(pid_proceso_a_finalizar, socket_kernel);
+			log_info(logger_memoria, "enviada respuesta de FINALIZAR_PROCESO_RTA \n");
+			break;
+
+		case INICIAR_HILO:
+			log_info(logger_memoria, "Recibí INICIAR_HILO \n");
+			valores = recibir_paquete(socket_kernel);
+			usleep(cfg_memoria->RETARDO_RESPUESTA * 1000);
+			log_info(logger_memoria, "enviada respuesta de INICIAR_HILO_RTA \n");
+			break;
+
+		case FINALIZAR_HILO:
+			log_info(logger_memoria, "Recibí FINALIZAR_HILO \n");
+			valores = recibir_paquete(socket_kernel);
+			usleep(cfg_memoria->RETARDO_RESPUESTA * 1000);
+			log_info(logger_memoria, "enviada respuesta de FINALIZAR_HILO_RTA \n");
+			break;
+
+		case PEDIDO_MEMORY_DUMP:
+			log_info(logger_memoria, "Recibí PEDIDO_MEMORY_DUMP \n");
+			valores = recibir_paquete(socket_kernel);
+			usleep(cfg_memoria->RETARDO_RESPUESTA * 1000);
+			log_info(logger_memoria, "enviada respuesta de PEDIDO_MEMORY_DUMP_RTA \n");
+			break;
+
 		case -1:
-			log_error(logger_memoria, "Kernel se desconecto. Terminando servidor.");
+			log_error(logger_memoria, "Kernel se desconecto. Terminando servidor.\n");
 			//return EXIT_FAILURE;
 			break;
 
