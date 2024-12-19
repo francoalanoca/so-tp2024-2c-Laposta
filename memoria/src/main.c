1d09e4f934f96467138902e0ab242cec07d16540
valendelrizzo <vdelrizzo@frba.utn.edu.ar>
2024-12-19 23:25:11 +0000

    tenemos 45 minutos

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -5,6 +5,7 @@ int main(int argc, char* argv[]) {
     char* path_config = argv[1];              //para correr por consola
     //char* path_config = "./memoria.config";   //para correr por vsc(debug)
 
+    char* ip_memoria = argv[2];                //para correr por consola
     
     //-------------------Configuraciones---------------------------
     if (!init(path_config) || !cargar_configuracion(path_config)) {
@@ -21,7 +22,7 @@ int main(int argc, char* argv[]) {
     log_trace(logger_memoria, "Se inicio correctamente la Memoria");
 
     //-------------------Servidores------------------------
-    iniciar_servidores();
+    iniciar_servidores(ip_memoria);
     log_trace(logger_memoria, "Se inicio correctamente los servidores");
 
     //void cerrar_programa();
